e23d3a374b2a66c6c1dc8122e24d8607e2bba874
ThaiHoa1402 <110773925+kurousagi110@users.noreply.github.com>
2023-11-14 11:12:49 +0700

    css web

--- public/dist/js/add.js
+++ public/dist/js/add.js
@@ -1,14 +1,25 @@
-let count = 2 ;
-function themTruongInput() {
+let count = 1;
 
-    let label = count++;
+function themTruongInput() {
+    const dem= count++;
+    const labels = ["Size", "Giá", "Giảm giá"];
     const container = document.getElementById('ThemMang');
-    const newInput = document.createElement('div');
-    newInput.classList.add('form-group');
-    newInput.innerHTML = `
-    <h6>${label}</h6>
-      <input type="text" class="form-control" placeholder="">
-    `;
-    
-    container.appendChild(newInput);
-  }
+    const newtitle = document.createElement('div');
+    newtitle.innerHTML = `
+                          <hr>
+                          <h6>${dem}</h6>`;
+    container.appendChild(newtitle);
+    for (let i = 0; i < 3; i++) {
+        const label = labels[i];
+        const newInput = document.createElement('div');
+        
+        newInput.classList.add('form-group');
+        newInput.innerHTML = `
+            <h6>${label}</h6>
+            <input type="text" class="form-control" placeholder="${label}">
+        `;
+        
+        container.appendChild(newInput);
+    }
+
+}
