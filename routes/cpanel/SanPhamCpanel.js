532900b85e444135f70d4cfaf4c3c69a7f351311
ThaiHoa1402 <110773925+kurousagi110@users.noreply.github.com>
2023-11-21 17:51:21 +0700

    cmd

--- routes/cpanel/SanPhamCpanel.js
+++ routes/cpanel/SanPhamCpanel.js
@@ -6,7 +6,12 @@ const jwt = require("jsonwebtoken");
 const upload = require("../../components/MiddleWare/uploadMultiFile");
 const loaiSanPhamController = require("../../components/LoaiSanPham/ControllerLoaiSanPham");
 const Swal = require("sweetalert2");
-const {uploadImageToS3} = require('../cpanel/uploadImageToS3FromClient');
+const { uploadImageToS3 } = require('../cpanel/uploadImageToS3FromClient');
+const {
+  addNotificationToAllUser,
+  sendNotificationNewProduct,
+} = require("../../components/Notification/ServiceNotification");
+
 
 //http://localhost:3000/cpanel/san-pham
 router.get("/", [AuthenWeb], async function (req, res, next) {
@@ -60,7 +65,7 @@ router.post(
               return null; // or handle the error in a way that suits your application
             }
           }));
-  
+
           console.log("All uploads completed. Keys:", keys);
           hinh_anh_sp = keys
         } catch (error) {
@@ -70,102 +75,137 @@ router.post(
 
       const result_loai_san_pham = await loaiSanPhamController.layLoaiSanPhamTheoId(loai_san_pham);
 
-        const san_pham = {
-            ten_san_pham,
-            result_loai_san_pham,
-            size: sizes,
-            mo_ta,
-            hinh_anh_sp,
-        };
+      const san_pham = {
+        ten_san_pham,
+        result_loai_san_pham,
+        size: sizes,
+        mo_ta,
+        hinh_anh_sp,
+      };
 
 
-        const sanpham = await sanphamController.themSanPhamAll(san_pham);
+      const sanpham = await sanphamController.themSanPhamAll(san_pham);
 
-        if (sanpham) {
-            res.status(200).redirect('/cpanel/san-pham');
+      if (sanpham) {
+        const resultSendNotification = await sendNotificationNewProduct(san_pham);
+        const resultAddNotification = await addNotificationToAllUser({
+          image: hinh_anh_sp,
+          title: san_pham.ten_san_pham,
+          id_product: result._id,
+          message: "Coffee.Love vừa cho ra sản phẩm mới đó",
+          type: "NewProduct",
+        });
+
+        if (!resultSendNotification) {
+          console.log("FAIL TO FIREBASE SEND NEW MESSAGE TO ALL DEVICE");
         } else {
-            const loai_san_pham = await loaiSanPhamController.layTatCaLoaiSanPham();
-            res.status(300).render('sanpham/themsanpham' , { loai_san_pham });
+          console.log("SUCCESS TO FIREBASE SEND NEW MESSAGE TO ALL DEVICE");
         }
-    } catch (err) {
-        console.log(err);
+        if (!resultAddNotification) {
+          console.log("FAIL TO ADD NOTIFICATION TO ALL USER");
+        } else {
+          console.log("SUCCESS TO ADD NOTIFICATION TO ALL USER");
+        }
+        res.status(200).redirect('/cpanel/san-pham');
+      } else {
         const loai_san_pham = await loaiSanPhamController.layTatCaLoaiSanPham();
-        res.status(400).render('sanpham/themsanpham' , { loai_san_pham });
+        res.status(300).render('sanpham/themsanpham', { loai_san_pham });
+      }
+    } catch (err) {
+      console.log(err);
+      const loai_san_pham = await loaiSanPhamController.layTatCaLoaiSanPham();
+      res.status(400).render('sanpham/themsanpham', { loai_san_pham });
     }
-});
+  });
 
 //sửa sản phẩm
 //http://localhost:3000/cpanel/san-pham/sua-san-pham/:id
 router.get('/sua-san-pham/:id', [AuthenWeb], async function (req, res, next) {
-    try {
-        const id = req.params.id;
-        let san_pham = await sanphamController.getSanPhamById(id);
-        const loai_san_pham = await loaiSanPhamController.layTatCaLoaiSanPham();
-        for(let i = 0; i < san_pham.size.length; i++){
-            san_pham.size[i].stt = i+1;
-        }        
-        res.status(200).render('sanpham/suasanpham', { san_pham, loai_san_pham });
-    } catch (err) {
-        console.log(err);
-        res.status(500).render('sanpham/suasanpham');
+  try {
+    const id = req.params.id;
+    let san_pham = await sanphamController.getSanPhamById(id);
+    const loai_san_pham = await loaiSanPhamController.layTatCaLoaiSanPham();
+    for (let i = 0; i < san_pham.size.length; i++) {
+      san_pham.size[i].stt = i + 1;
     }
+    res.status(200).render('sanpham/suasanpham', { san_pham, loai_san_pham });
+  } catch (err) {
+    console.log(err);
+    res.status(500).render('sanpham/suasanpham');
+  }
 });
 
 //sửa sản phẩm
 //http://localhost:3000/cpanel/san-pham/sua-san-pham/:id
 router.post('/sua-san-pham/:id', [AuthenWeb, upload], async function (req, res, next) {
-    try {
-        let { ten_san_pham, loai_san_pham, mo_ta } = req.body;
-        let files = req.files; // 'files' since you used upload.array
-        let id = req.params.id;
-        console.log("size", req.body.sizes);
-        let sizes = JSON.parse(req.body.sizes);
-        let hinh_anh_sp = [];
-        if (files && files.length > 0) {
-            hinh_anh_sp = files.map(file => ({
-                hinh_anh_sp: `https://coffee.thaihoa.software/images/${encodeURIComponent(file.filename)}`
-            }));
+  try {
+    let { ten_san_pham, loai_san_pham, mo_ta } = req.body;
+    let files = req.files; // 'files' since you used upload.array
+    let id = req.params.id;
+    console.log("size", req.body.sizes);
+    let sizes = JSON.parse(req.body.sizes);
+    let hinh_anh_sp = [];
+    if (files && files.length > 0) {
+      try {
+          const keys = await Promise.all(files.map(async (file) => {
+            try {
+              const uploadResult = await uploadImageToS3(file.path);
+              return uploadResult;
+            } catch (error) {
+              console.error(`Error uploading file ${file.originalname}:`, error);
+              return null; // or handle the error in a way that suits your application
+            }
+          }));
+
+          console.log("All uploads completed. Keys:", keys);
+          hinh_anh_sp = keys
+        } catch (error) {
+          console.log("Error:", error);
         }
-
-        const result_loai_san_pham = await loaiSanPhamController.layLoaiSanPhamTheoId(loai_san_pham);
-
-        const san_pham = {
-            ten_san_pham,
-            result_loai_san_pham,
-            mo_ta,
-            hinh_anh_sp,
-            sizes: sizes
-        };
-        console.log("san_pham", san_pham);
-        const sanpham = await sanphamController.suaSanPhamAll(id, san_pham);
-
-        if (sanpham) {
-            res.status(200).redirect('/cpanel/san-pham');
-        } else {
-            res.status(300).render('sanpham/suasanpham');
-        }
-    } catch (err) {
-        console.log(err);
-        res.status(400).render('sanpham/suasanpham');
     }
+
+    const result_loai_san_pham = await loaiSanPhamController.layLoaiSanPhamTheoId(loai_san_pham);
+    // gửi thông báo có sản phẩm mới cho tất cả thiết bị
+
+
+
+    const san_pham = {
+      ten_san_pham,
+      result_loai_san_pham,
+      mo_ta,
+      hinh_anh_sp,
+      sizes: sizes
+    };
+    console.log("san_pham", san_pham);
+    const sanpham = await sanphamController.suaSanPhamAll(id, san_pham);
+
+    if (sanpham) {
+      res.status(200).redirect('/cpanel/san-pham');
+    } else {
+      res.status(300).render('sanpham/suasanpham');
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).render('sanpham/suasanpham');
+  }
 });
 
 //xóa sản phẩm
 //http://localhost:3000/cpanel/san-pham/xoa-san-pham/:id
 router.get('/xoa-san-pham/:id', [AuthenWeb], async function (req, res, next) {
-    try {
-      const id = req.params.id;
-      const sanpham = await sanphamController.xoaSanPham(id);
+  try {
+    const id = req.params.id;
+    const sanpham = await sanphamController.xoaSanPham(id);
 
-      if (sanpham) {
-        res.status(200).json({ result: 'success' });
-      } else {
-        res.status(200).json({ result: 'failure' });
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(400).json({ result: 'error' });
+    if (sanpham) {
+      res.status(200).json({ result: 'success' });
+    } else {
+      res.status(200).json({ result: 'failure' });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ result: 'error' });
+  }
 });
 
 //sửa sản phẩm
